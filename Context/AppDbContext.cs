dd664ae526d228bba53a26ceae19fe3f7a711277
MatheusAmaralDeSousa <104398413+MatheusAmaralDeSousa@users.noreply.github.com>
2025-03-26 21:56:12 -0300

    feat: Context added

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Context;
+
+public class AppDbContext : DbContext
+{
+    protected readonly IConfiguration _configuration;
+        
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseNpgsql(_configuration.GetConnectionString("ApiCatalogo"));
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
+
+}
